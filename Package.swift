31e6c336fc1a1bccd07a8181d4f836ce8219e854
tanaka <lemo.nade.engineer.room@gmail.com>
2025-02-23 00:47:24 +0900

    feat: ✨ パッケージ定義を更新し新しい依存関係を追加 - 新しい依存関係をPackage.swiftに追加 - ライブラリ名やターゲット名を修正 - 新しいマクロ用ターゲットと設定を追加

--- Package.swift
+++ Package.swift
@@ -1,24 +1,64 @@
 // swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
+import CompilerPluginSupport
 import PackageDescription
 
 let package = Package(
     name: "event-store-adapter-swift-support",
+    platforms: [.macOS(.v15)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "event-store-adapter-swift-support",
-            targets: ["event-store-adapter-swift-support"])
+            name: "EventStoreAdapterSupport",
+            targets: ["EventStoreAdapterSupport"])
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/lemo-nade-room/event-store-adapter-swift.git", branch: "main"),
+        .package(url: "https://github.com/swiftlang/swift-syntax", from: "600.0.0"),
+        .package(url: "https://github.com/pointfreeco/swift-macro-testing.git", from: "0.6.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "event-store-adapter-swift-support"),
-        .testTarget(
-            name: "event-store-adapter-swift-supportTests",
-            dependencies: ["event-store-adapter-swift-support"]
+            name: "EventStoreAdapterSupport",
+            dependencies: [
+                .product(name: "EventStoreAdapter", package: "event-store-adapter-swift"),
+                "EventStoreAdapterSupportMacro",
+            ],
+            swiftSettings: swiftSettings
         ),
-    ]
+        .testTarget(
+            name: "EventStoreAdapterSupportTests",
+            dependencies: ["EventStoreAdapterSupport"],
+            swiftSettings: swiftSettings
+        ),
+        .macro(
+            name: "EventStoreAdapterSupportMacro",
+            dependencies: [
+                .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
+                .product(name: "SwiftCompilerPlugin", package: "swift-syntax"),
+            ],
+            swiftSettings: swiftSettings
+        ),
+        .testTarget(
+            name: "EventStoreAdapterSupportMacroTests",
+            dependencies: [
+                "EventStoreAdapterSupportMacro",
+                .product(name: "SwiftSyntaxMacroExpansion", package: "swift-syntax"),
+                .product(name: "MacroTesting", package: "swift-macro-testing"),
+            ],
+            swiftSettings: swiftSettings
+        ),
+    ],
+    swiftLanguageModes: [.v6]
 )
+
+var swiftSettings: [SwiftSetting] {
+    [
+        .enableUpcomingFeature("ExistentialAny"),
+        .enableUpcomingFeature("InternalImportsByDefault"),
+    ]
+}
